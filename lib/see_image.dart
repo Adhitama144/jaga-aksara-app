a2fd4eef22853e49511de0fb0c43e08c48d07a7b
Adhitama144 <adhitamaafriansyah@gmail.com>
2024-10-22 17:17:29 +0700

    Desain See_image rampung

---
+++ lib/see_image.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SeeImage extends StatefulWidget {
+  const SeeImage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SeeImageState();
+  }
+}
+
+class _SeeImageState extends State<SeeImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          color: Colors.white,
+        ),
+        title: const Text(
+          'See Image Full',
+          style: TextStyle(
+              color: Colors.white, fontSize: 32, fontFamily: 'AkayaTelivigala'),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 183, 3),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.arrow_downward),
+        backgroundColor: Color.fromARGB(255, 255, 183, 3),
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image(
+          image: NetworkImage('https://picsum.photos/500/500'),
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
