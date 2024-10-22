f3460980a49fbaaad8d9a737eaf55675f2715a63
Adhitama144 <adhitamaafriansyah@gmail.com>
2024-10-22 16:54:17 +0700

    Desain read rampung

---
+++ lib/read.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Read extends StatefulWidget {
+  const Read({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ReadState();
+  }
+}
+
+class _ReadState extends State<Read> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Read',
+          style: TextStyle(
+              color: Colors.white, fontSize: 32, fontFamily: 'AkayaTelivigala'),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 183, 3),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.volume_up),
+        backgroundColor: Color.fromARGB(255, 255, 183, 3),
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Image(
+                image: NetworkImage(
+                    'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png'),
+              ),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta pretium tincidunt. Suspendisse quam quam, sodales a purus sit amet, porta luctus lorem. Praesent eget dapibus mauris. Aenean quis egestas orci, non laoreet libero. Nulla quis augue rhoncus, pulvinar quam sit amet, dapibus nulla. Suspendisse potenti. Curabitur et tortor id eros faucibus gravida ac eget turpis. Sed facilisis magna nisi. Donec rhoncus, ante eu euismod interdum, nisl nunc consequat tortor, a consectetur augu.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'AkayaTelivigala',
+                  color: Color.fromARGB(255, 22, 41, 56),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
